cbd6d72703bad39f2fef9b14bb57f1be40f11950
mrdjanz97 <161601158+mrdjanz97@users.noreply.github.com>
2025-02-27 18:04:39 +0100

    Add files via upload

---
+++ database/table.names.ts
@@ -0,0 +1,13 @@
+export const PROFILES_TABLE: string = 'profiles';
+export const FEEDBACKS_TABLE: string = 'feedbacks';
+export const FEEDBACK_REACTIONS_TABLE: string = 'feedback_reactions';
+export const GET_FEEDBACKS_WITH_REACTIONS: string = 'get_feedbacks_with_reactions';
+export const GET_FEEDBACKS_STATUS_COUNT = 'get_feedback_counts_by_status';
+export const COMPANY_RESOURCES = 'company_resources';
+export const NOTIFICATIONS_TABLE = 'notifications';
+export const FEEDBACK_COMMENTS_TABLE = 'feedback_comments';
+export const NEW_NOTIFICATIONS_COUNT_RPC = 'get_new_notifications_count';
+
+export const DEV_BUCKET = 'dev';
+export const RESOURCES_BUCKET_FOLDER = 'resources';
+export const AVATARS_BUCKET_FOLDER = 'avatars';
